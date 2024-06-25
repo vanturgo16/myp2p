0d1c7720f94d6c156f4c7e7cea665322257befa4
Hardy Prabowo <hardy.prabowo@napinfo.co.id>
2024-06-25 23:52:24 +0700

    change notif loan to transaction

--- resources/views/lenders/history_balance.blade.php
+++ resources/views/lenders/history_balance.blade.php
@@ -116,7 +116,7 @@
                                     <form action="{{ url('/lender/balance/approved', encrypt($data->id)) }}" method="POST" style="display:inline-block;">
                                         @csrf
                                         @method('patch')
-                                        <button type="submit" class="btn btn-success btn-xs" onclick="return confirm('Are You Sure APPROVED this Loan?')">
+                                        <button type="submit" class="btn btn-success btn-xs" onclick="return confirm('Are You Sure APPROVED this Transaction?')">
                                             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-file-like"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 16m0 1a1 1 0 0 1 1 -1h1a1 1 0 0 1 1 1v3a1 1 0 0 1 -1 1h-1a1 1 0 0 1 -1 -1z" /><path d="M6 20a1 1 0 0 0 1 1h3.756a1 1 0 0 0 .958 -.713l1.2 -3c.09 -.303 .133 -.63 -.056 -.884c-.188 -.254 -.542 -.403 -.858 -.403h-2v-2.467a1.1 1.1 0 0 0 -2.015 -.61l-1.985 3.077v4z" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 12.1v-7.1a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-2.3" /></svg>
                                             Approved
                                         </button>
@@ -124,7 +124,7 @@
                                     <form action="{{ url('/lender/balance/rejected', encrypt($data->id)) }}" method="POST" style="display:inline-block;">
                                         @csrf
                                         @method('patch')
-                                        <button type="submit" class="btn btn-danger btn-xs" onclick="return confirm('Are You Sure REJECTED this Loan?')">
+                                        <button type="submit" class="btn btn-danger btn-xs" onclick="return confirm('Are You Sure REJECTED this Transaction?')">
                                             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-file-dislike"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 14m0 1a1 1 0 0 1 1 -1h1a1 1 0 0 1 1 1v3a1 1 0 0 1 -1 1h-1a1 1 0 0 1 -1 -1z" /><path d="M6 15a1 1 0 0 1 1 -1h3.756a1 1 0 0 1 .958 .713l1.2 3c.09 .303 .133 .63 -.056 .884c-.188 .254 -.542 .403 -.858 .403h-2v2.467a1.1 1.1 0 0 1 -2.015 .61l-1.985 -3.077v-4z" /><path d="M14 3v4a1 1 0 0 0 1 1h4" /><path d="M5 11v-6a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2h-2.5" /></svg>
                                             Rejected
                                         </button>
